5003674e17eb309549d517473db1274267c06381
Michael Knoch <info@michaelknoch.de>
2016-04-26 15:38:04 +0200

    improve login comp

--- app/comp/login/login.comp.ts
+++ app/comp/login/login.comp.ts
@@ -1,39 +1,48 @@
 import { Component, OnInit } from 'angular2/core';
 import {Router} from 'angular2/router';
 import {UserService} from "../../service/user/user.service";
+import {Modal} from 'angular2-modal';
+
 declare var __moduleName: any;
 
 @Component({
     moduleId: __moduleName,
     templateUrl: 'template.html',
-    styleUrls: ['login.css']
+    styleUrls: ['login.css'],
+    providers: [Modal],
 })
 
 export class Login {
 
     _userService;
-
-    login: Boolean = true;
+    loginState: Boolean = true;
 
     password: String;
     mail: String;
     name: String;
     surname: String;
 
-    constructor(private _router: Router, private _userService: UserService) {
+    constructor(private modal: Modal, private _router: Router, private _userService: UserService) {
         this._userService = _userService
     }
 
-    userLogin() {
+    login() {
+
         this._userService.login(this.mail, this.password).subscribe(
             data => this._router.navigate(['Root']),
-            err => console.error(err));
+            err => this.err(err);
     }
 
-    userRegister() {
+    register() {
         this._userService.register(this.mail, this.password, this.name, this.surname).subscribe(
             data => this._router.navigate(['Root']),
-            err => console.error(err));
+            err => this.err(err);
+    }
+
+    err(msg) {
+        console.error(msg);
+        this.modal.alert()
+            .title('Hoppla').body(msg.status + ':' + msg.statusText).open();
     }
 
 }
