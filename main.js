48e9a8a92cd94f285fb22fe371e8a3deccd723f4
Michael Knoch <info@michaelknoch.de>
2016-04-26 22:12:30 +0200

    more space

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 950, height: 700});
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
