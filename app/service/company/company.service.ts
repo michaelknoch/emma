ff85d2d0f024aa4f00db226237a0265fedce5211
Michael Knoch <info@michaelknoch.de>
2016-04-26 14:01:34 +0200

    add company service layer

---
+++ app/service/company/company.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx';
+import {Config} from "../../app.config";
+
+@Injectable()
+export class CompanyService {
+
+    http;
+
+    constructor(http: Http) {
+        this.http = http;
+    }
+
+    getCompanies() {
+        return this.http.get(Config.BASEPATH + 'companies')
+            .map(res => res.json())
+    }
+
+    createCompanie(mail: String, name: String, url: String, address: any) {
+        return this.http.post(Config.BASEPATH + 'users', JSON.stringify({
+                mail: mail,
+                name: name,
+                url: url,
+                address: address
+            }))
+            .map(res => res.json())
+    }
+
+}
