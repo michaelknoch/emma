249d4f563070eee374384e0c93a6ba09043845d5
Michael Knoch <info@michaelknoch.de>
2016-04-26 22:54:43 +0200

    share data

--- app/comp/companyList/companyList.comp.ts
+++ app/comp/companyList/companyList.comp.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core'
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {CompanyService} from "../../service/company/company.service";
 import {Router} from "angular2/router";
+import {DataService} from "../../service/data.service";
 declare var __moduleName: any;
 
 @Component({
@@ -16,7 +17,7 @@ export class CompanyList {
 
     companies;
 
-    constructor(private companyService: CompanyService, private _router: Router) {
+    constructor(private companyService: CompanyService, private _router: Router, private _dataService: DataService) {
         companyService.getCompanies().subscribe(
             data => {
                 this.companies = data;
@@ -27,7 +28,8 @@ export class CompanyList {
     }
 
     selectCompany(company) {
-        this.companyService.selectCompany(company.id).subscribe(data => {
+        this.companyService.selectCompany(company._id).subscribe(data => {
+            this._dataService.setData('current-company', company);
             console.info('select company', data);
             this._router.navigate(['Root'])
         });
