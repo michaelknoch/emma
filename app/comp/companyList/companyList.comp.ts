700e4ecf993f0cc9900c98c609dfd246a701b981
Michael Knoch <info@michaelknoch.de>
2016-04-26 16:01:07 +0200

    init companyList comp

---
+++ app/comp/companyList/companyList.comp.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core'
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+declare var __moduleName: any;
+
+@Component({
+    moduleId: __moduleName,
+    selector: 'company-list',
+    directives: [ROUTER_DIRECTIVES],
+    templateUrl: 'companyList.html',
+    styleUrls: ['companyList.css'],
+})
+
+export class CompanyList {
+
+    constructor() {}
+
+}
