34683600207fa0992a2eddee1e3389b3e516af6b
Michael Knoch <info@michaelknoch.de>
2016-05-10 16:57:23 +0200

    stuff

--- app/main.ts
+++ app/main.ts
@@ -3,13 +3,16 @@
 import {AppComponent} from './app.component';
 import {provide} from 'angular2/core';
 import {bootstrap}    from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy} from 'angular2/platform/common';
+
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {CostumerService} from './service/costumer/costumer.service';
 import {UserService} from "./service/user/user.service";
 import {ModalConfig} from 'angular2-modal';
 import {CompanyService} from "./service/company/company.service";
 import {DataService} from "./service/data.service";
+
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
